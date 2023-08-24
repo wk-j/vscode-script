c86cadda4f0fa80df7bb3dc20d8a0df931c275f3
wk <somnuk.wk@gmail.com>
2023-08-24 07:52:40 +0700

    Added monaco styles and cursor updates
    A new settings.json file for zed has been introduced. Added .monaco-mouse-cursor-text and .monaco-workbench CSS rules in the styles.css file. Also adjusted color, cursor style and trail length for the cursor trial in index.js.

--- cursor-trial/index.js
+++ cursor-trial/index.js
@@ -3,7 +3,8 @@
 // Configuration
 
 // Set the color of the cursor trail to match the user's cursor color
-const Color = "#A052FF" // If set to "default," it will use the theme's cursor color.
+// const Color = "#A052FF" // If set to "default," it will use the theme's cursor color.
+const Color = "orange"
 // ! default will only reference editorCursor.background
 // "workbench.colorCustomizations": {
 //     "editorCursor.background": "#A052FF",
@@ -11,10 +12,11 @@ const Color = "#A052FF" // If set to "default," it will use the theme's cursor c
 
 // Set the style of the cursor to either a line or block
 // line option use fill() to draw trail, block option use lineTo to draw trail
-const CursorStyle = "block" // Options are 'line' or 'block'
+// const CursorStyle = "block" // Options are 'line' or 'block'
+const CursorStyle = "block"
 
 // Set the length of the cursor trail. A higher value may cause lag.
-const TrailLength = 8 // Recommended value is around 8
+const TrailLength = 2 // Recommended value is around 8
 
 // Set the polling rate for handling cursor created and destroyed events, in milliseconds.
 const CursorUpdatePollingRate = 500 // Recommended value is around 500
